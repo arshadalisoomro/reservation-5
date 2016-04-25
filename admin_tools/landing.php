fee2d1a309f3cd084b8c54122a838023bd3144b4
jdfless <jdfless@noemail.com>
2016-04-25 15:56:34 -0700

    removing jquery dep in landing.php

--- admin_tools/landing.php
+++ admin_tools/landing.php
@@ -5,8 +5,6 @@
 <head>
     <title>Admin Landing</title>
     <meta charset="UTF-8">
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" type="text/css" href="../css/boatRes.css">
 </head>
 <body>
