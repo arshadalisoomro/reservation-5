225ec65e61d0251d3945fb74f6c6cbf220f552b7
Suefle59 <suefle@hotmail.com>
2016-04-25 22:39:26 -0700

    admin main selection page after login, was admin_form.html

---
+++ admin_tools/admin_form.php
@@ -0,0 +1,39 @@
+
+<?php
+//require valid admin login
+require 'check_login.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Selection Form</title>
+    <meta charset = "UTF-8">
+    <link rel="stylesheet" type="text/css" href="../css/admin.css">
+</head>
+<body>
+    <div>
+
+        <legend><b>Admin Selections:</b></legend>
+        <br />
+        <br />
+        <a id=link href="manifestSelect.php">View Manifest</a>
+        <br />
+        <br />
+        <a id=link href="futureRes.php">View all future reservations</a>
+        <br />
+        <br />
+        <a id=link href="pastRes.php">View all past reservations</a>
+        <br />
+        <br />
+        <a id=link href="specialBoatselect.php">Add a non-scheduled boat</a>
+        <br />
+        <br />
+        <a id=link href="cancelBoat.html">Cancel a boat</a>
+        <br />
+        <br />
+        <a id=link href="editEmail.html">Edit the email text</a>
+
+    </div>
+</body>
+</html>
