73a886b222d2ee62280f4902ef8ff61764a585c1
Suefle59 <suefle@hotmail.com>
2016-04-25 22:48:25 -0700

    moved file to admin_tools

---
+++ admin_tools/getManifestBoats.php
@@ -0,0 +1,37 @@
+<?php ini_set('display_errors', 'On'); ?>
+<?php
+
+//returns all non-cancelled boats in the boat database for viewing/printing manifests
+
+//connect to sql server with global $conn
+require '../dbConnect.php';
+
+date_default_timezone_set('America/Los_Angeles');
+
+$today = date("Y-m-d");
+
+$date = new DateTime($today);
+$date->modify('+30 day');
+
+$maxDay = $date->format("Y-m-d");
+
+$qry = $conn->prepare("SELECT DISTINCT departDate FROM boatsDNW
+      WHERE isCancelled = 0");
+
+$qry->execute(array($today, $maxDay));
+
+$outDate = $qry->fetchAll();
+
+$boatArray = array();
+
+foreach($outDate as $row) {
+    $boatArray[] = $row['departDate'];
+}
+
+$json = json_encode($boatArray);
+echo $json;
+
+//close connection
+$conn = null;
+
+?>
