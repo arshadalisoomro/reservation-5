8dd2c1971ac3fb0ecd26eb9630961a9e938f2cec
Suefle59 <suefle@hotmail.com>
2016-04-27 13:04:38 -0700

    added one radio button and fixed output text

--- admin_tools/specialBoatSelect.php
+++ admin_tools/specialBoatSelect.php
@@ -36,25 +36,29 @@ require 'check_login.php';
 
 <form action="specialBoatConfirm.php" method="post" onsubmit="return whenSubmit()">
 
-<p>Select date for non-scheduled boat: <input type="text" id="specBoatDate" name="specBoatDate"></p>
+<p>Select date for non-scheduled boat:
+  <br/>
+  <br/>
+  <input type="text" id="specBoatDate" name="specBoatDate"></p>
 <div id="boatDateReq">Select a date for the non-scheduled boat.</div>
 
 
-<p>Select time From Decatur to Anacortes:
+<p>Select time for non-scheduled boat:
 <br />
-(boat From Anacortes to Decatur will automatically be generated)</p>
+<br />
 
-  <input type="radio" name="time" value="10:00:00" checked> 10am<br>
-  <input type="radio" name="time" value="11:00:00"> 11am<br>
-  <input type="radio" name="time" value="12:00:00"> 12pm<br>
-  <input type="radio" name="time" value="13:00:00"> 1pm<br>
-  <input type="radio" name="time" value="14:00:00"> 2pm<br>
-  <input type="radio" name="time" value="15:00:00"> 3pm<br>
-  <input type="radio" name="time" value="16:00:00"> 4pm<br>
-  <input type="radio" name="time" value="17:00:00"> 5pm<br>
-  <input type="radio" name="time" value="18:00:00"> 6pm<br>
-  <input type="radio" name="time" value="19:00:00"> 7pm<br>
-  <input type="radio" name="time" value="19:30:00"> 7:30pm<br>
+  <input type="radio" name="time" value="10:00:00" checked> 10am to Anacortes, 9am to DNW<br>
+  <input type="radio" name="time" value="11:00:00"> 11am to Anacortes, 10am to DNW<br>
+  <input type="radio" name="time" value="12:00:00"> 12pmto Anacortes, 11am to DNW<br>
+  <input type="radio" name="time" value="13:00:00"> 1pm to Anacortes, 12pm to DNW<br>
+  <input type="radio" name="time" value="14:00:00"> 2pm to Anacortes, 1pm to DNW<br>
+  <input type="radio" name="time" value="15:00:00"> 3pm to Anacortes, 2pm to DNW<br>
+  <input type="radio" name="time" value="16:00:00"> 4pm to Anacortes, 3pm to DNW<br>
+  <input type="radio" name="time" value="17:00:00"> 5pm to Anacortes, 4pm to DNW<br>
+  <input type="radio" name="time" value="18:00:00"> 6pm to Anacortes, 5pm to DNW<br>
+  <input type="radio" name="time" value="19:00:00"> 7pm to Anacortes, 6pm to DNW<br>
+  <input type="radio" name="time" value="19:30:00"> 7:30pm to Anacortes, 6:30pm to DNW<br>
+  <input type="radio" name="time" value="20:30:00"> 8:30pm to Anacortes, 7:30pm to DNW<br>
   <br>
   <input type="submit" name="submit" value="Submit">
 </form>
