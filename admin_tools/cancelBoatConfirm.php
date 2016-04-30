fa63962c8680cb3c451ffc5a5a1c38dd91a93215
Suefle59 <suefle@hotmail.com>
2016-04-29 17:30:13 -0700

    confirm the boat to cancel that was selected in cancelBoatSelect

---
+++ admin_tools/cancelBoatConfirm.php
@@ -0,0 +1,66 @@
+<?php ini_set('display_errors', 'On'); ?>
+<?php
+
+//this script adds a non-scheduled boat
+
+//connect to sql server with global $conn
+require '../dbConnect.php';
+
+//set timezone to west coast
+date_default_timezone_set('America/Los_Angeles');
+
+if (isset($_POST['cancelBoatDate'])) {
+    $dateChosen = $_POST['cancelBoatDate'];
+    $timeChosen = $_POST['cancelBoatTime'];
+}
+
+//start php session
+session_start();
+//set php session var
+$_SESSION['dateChosen'] = $dateChosen;
+$_SESSION['timeChosen'] = $timeChosen;
+
+//close the connection
+$conn = null;
+
+?>
+
+<html>
+    <head>
+        <title>Confirm Boat to Cancel</title>
+        <meta charset = "UTF-8"></meta>
+            <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'></link>
+        <link rel="stylesheet" type="text/css" href="../css/admin.css"></link>
+
+     </head>
+     <body>
+       <a>Please confirm that you would like to cancel the following boat:</a>
+       <br/>
+       <br/>
+       <?php
+         $newDateChosen = date('l, m/d/Y', strtotime($dateChosen));
+         echo $newDateChosen;
+       ?>
+       <br />
+       <br />
+       <a>Departing Anacortes at</a>
+       <?php
+       $newTimeChosen = date('g:ia', strtotime($timeChosen));
+       echo $newTimeChosen;
+       ?>
+       <br />
+       <br />
+       <p>
+       <!--<form id="confirm action="specialBoatAdd.php" method="post">-->
+       <form action="cancelBoat.php" method="post">
+       <input type="submit" name="submit" value="Confirm and Cancel Boat"></input>
+       </form>
+       <br/>
+       <br/>
+       <form action="admin_form.php">
+       <input type="submit" name="submit" value="Abort and Return"></input>
+       </form>
+       </p>
+</body>
+</html>
