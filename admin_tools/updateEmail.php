33a633383f155ec828625542d63342acdbf3ec4a
Suefle59 <suefle@hotmail.com>
2016-05-02 14:27:57 -0700

    new file for updating AppData database with new email response message

---
+++ admin_tools/updateEmail.php
@@ -0,0 +1,51 @@
+<?php ini_set('display_errors', 'On'); ?>
+<?php
+
+//set url variables for redirect if no POST
+$admin_url="https://www.flessner.org/-jonTest/editEmail.php";
+
+//connect to sql server with global $conn
+if($_POST){
+   require '../dbConnect.php';
+   $emailComment = $_POST['comment'];
+   }
+  else{
+    header("Location: $admin_url");
+    die();
+}
+
+$qry = $conn->prepare("UPDATE  AppData
+                       SET RespondMsg = '$emailComment'
+                       WHERE ID = 1");
+
+$qry->execute();
+
+$conn = null;
+
+?>
+
+<html>
+    <head>
+    <title>Update Email Response Message</title>
+    <meta charset = "UTF-8"></meta>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'></link>
+    <link rel="stylesheet" type="text/css" href="../css/admin.css"></link>
+
+     </head>
+     <body>
+            <div id=emailDiv>
+            <a>The email response message has been updated:</a>
+            <br />
+            <br />
+                <?php
+                  echo $emailComment
+                ?>
+            <br />
+            <br />
+            </div>
+            <form action="admin_form.php">
+            <input type="submit" name="submit" value="Return"></input>
+            </form>
+    </body>
+</html>
