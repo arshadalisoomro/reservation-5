bd26dfc752adea8337b43f0edc6b9c96a51f7dc9
Suefle59 <suefle@hotmail.com>
2016-05-18 22:32:41 -0700

    removed unnecessary reference to jquery library

--- admin_tools/editEmail.php
+++ admin_tools/editEmail.php
@@ -21,7 +21,7 @@ $conn = null;
     <head>
         <title>Edit Email Response Message</title>
         <meta charset = "UTF-8"></meta>
-            <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+      <!--  <script src="../libraries/jquery-2.2.3.min.js"></script> -->
         <link rel="stylesheet" type="text/css" href="../css/admin.css"></link>
     </head>
     <body>
