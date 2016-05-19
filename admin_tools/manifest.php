77736d76d6e72fb044764064fa54dbf83a8d4d6d
Suefle59 <suefle@hotmail.com>
2016-05-18 22:34:39 -0700

    removed unnecessary reference to jquery library

--- admin_tools/manifest.php
+++ admin_tools/manifest.php
@@ -51,7 +51,7 @@ $conn = null;
     <head>
         <title>Manifest Output</title>
         <meta charset = "UTF-8"></meta>
-            <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <!--    <script src="../libraries/jquery-2.2.3.min.js"></script> -->
         <link rel="stylesheet" type="text/css" href="../css/admin.css"></link>
 
      </head>
